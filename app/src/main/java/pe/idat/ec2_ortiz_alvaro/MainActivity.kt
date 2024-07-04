f9293bcc96da7b7f40b778bd3a59dbadd6b25b4e
ajosx <ajos1997.ao@gmail.com>
2024-07-03 20:38:30 -0500

    proyecto vacio

---
+++ app/src/main/java/pe/idat/ec2_ortiz_alvaro/MainActivity.kt
@@ -0,0 +1,47 @@
+package pe.idat.ec2_ortiz_alvaro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.idat.ec2_ortiz_alvaro.ui.theme.Ec2ortizalvaroTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ec2ortizalvaroTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Ec2ortizalvaroTheme {
+        Greeting("Android")
+    }
+}
